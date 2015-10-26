b731b3ba9f6047d944a4266d7f7a8b8fc95f7745
Steve <stevenlyall@gmail.com>
2015-10-26 10:58:07 -0700

    initial commit, skeleton from text

--- producerconsumer.c
+++ producerconsumer.c
@@ -6,7 +6,18 @@
 
 int main(int argc, char *argv[]) {
 /* 1. Get command line arguments argv[1],argv[2],argv[3] */
+    if (argc != 3) {
+        printf("Incorrect number of aguments. Usage:\n");
+        printf("producerconsumer <time to sleep before terminating> <number of producers> <number of consumers\n");
+        return -1;
+    }
+
+    int timeToSleep = argv[1];
+    int numProducerThreads = argv[2];
+    int numConsumerThreads = argv[3];
+
 /* 2. Initialize buffer */
+
 /* 3. Create producer thread(s) */
 /* 4. Create consumer thread(s) */
 /* 5. Sleep */
